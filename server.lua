628114e21d5b7f72c863c2f8ff8ab4d02ab8dcbb
Ty <101424789+wootyh@users.noreply.github.com>
2024-02-09 13:13:26 -0500

    Update server.lua

--- server.lua
+++ server.lua
@@ -1,13 +1,28 @@
-ESX.RegisterServerCallback('DE_scoreboard:getOnlinePlayers', function(source, cb)
-    local xPlayers = ESX.GetExtendedPlayers()
-    local data = {}
-
-    for _, xPlayer in pairs(xPlayers) do
-        table.insert(data, {
-            id = xPlayer.source,
-            label = '[' .. xPlayer.source .. '] ' .. GetPlayerName(xPlayer.source)
-        })
-    end
+if Shared.Core == 'esx' then
+    ESX.RegisterServerCallback('DE_scoreboard:getOnlinePlayers', function(source, cb)
+        local xPlayers = ESX.GetExtendedPlayers()
+        local data = {}
     
-    cb(data)
-end)
+        for _, xPlayer in pairs(xPlayers) do
+            table.insert(data, {
+                id = xPlayer.source,
+                label = '[' .. xPlayer.source .. '] ' .. GetPlayerName(xPlayer.source)
+            })
+        end
+        
+        cb(data)
+    end)
+elseif Shared.Core == 'qb' then
+    QBCore.Functions.CreateCallback('DE_scoreboard:getOnlinePlayers', function(source, cb)
+        local Player = QBCore.Functions.GetPlayer(source)
+        local data = {}
+    
+        for _, v in pairs(QBCore.Functions.GetQBPlayers()) do
+            table.insert(data, {
+                id = Player.PlayerData.source,
+                label = '[' .. Player.PlayerData.source .. '] ' .. GetPlayerName(Player.PlayerData.source)
+            })
+        end
+        cb(data)
+    end)
+end
