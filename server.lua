05c90a897f7fc038baa0b17ff6804452f88d3903
Ty <101424789+wootyh@users.noreply.github.com>
2024-03-20 00:29:47 -0400

    Update server.lua

--- server.lua
+++ server.lua
@@ -1,15 +1,37 @@
+----------------------------------------------------------------------
+-- Functions
+----------------------------------------------------------------------
+local HexIdToSteamId = function(hexId)
+    local cid = math.floor(tonumber(string.sub(hexId, 7), 16))
+    local steam64 = math.floor(tonumber(string.sub( cid, 2)))
+    local a = steam64 % 2 == 0 and 0 or 1
+    local b = math.floor(math.abs(6561197960265728 - steam64 - a) / 2)
+    local sid = "STEAM_0:"..a..":"..(a == 1 and b -1 or b)
+    return sid
+end
+----------------------------------------------------------------------
+-- Callbacks
+----------------------------------------------------------------------
 if Shared.Core == 'esx' then
     ESX.RegisterServerCallback('DE_scoreboard:getOnlinePlayers', function(source, cb)
         local xPlayers = ESX.GetExtendedPlayers()
         local data = {}
     
-        for _, xPlayer in pairs(xPlayers) do
-            table.insert(data, {
-                id = xPlayer.source,
-                label = '[' .. xPlayer.source .. '] ' .. GetPlayerName(xPlayer.source)
-            })
-        end
-        
+        if Config.Identifier then
+            for _, xPlayer in pairs(xPlayers) do
+                table.insert(data, {
+                    id = xPlayer.source,
+                    label = '[' .. xPlayer.source .. '] ' .. HexIdToSteamId(xPlayer.getIdentifier())
+                })
+            end
+        else
+            for _, xPlayer in pairs(xPlayers) do
+                table.insert(data, {
+                    id = xPlayer.source,
+                    label = '[' .. xPlayer.source .. '] ' .. GetPlayerName(xPlayer.source)
+                })
+            end
+        end        
         cb(data)
     end)
 elseif Shared.Core == 'qb' then
